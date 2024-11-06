d8d929233c069b222562c7681a1fb7aea013dd31
Marc Carlson <55961315+MarcRichardCarlson@users.noreply.github.com>
2024-11-06 13:43:38 +0100

    Update Portrait.tsx

--- components/HeroSection/Portrait.tsx
+++ components/HeroSection/Portrait.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
-import Portrait from '../../public/images/Portrait.png';
+import PortraitImage from '../../public/images/Portrait.png';
 
 const Portrait: React.FC = () => {
   return (
     <div className="z-50 w-24 h-24 sm:w-48 sm:h-48 md:w-48 md:h-48 lg:w-48 lg:h-48 xl:w-64 xl:h-64 relative overflow-hidden rounded-full bg-gradient-to-r from-green-800 via-green-600 to-green-400">
       <Image
-        src={Portrait}
+        src={PortraitImage}
         alt="Portrait"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
