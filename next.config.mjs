ce4639345a672ca75315ad45dbec042654288dcd
Marc Carlson <marc@nordnordic.se>
2024-11-07 00:02:04 +0100

    mjs change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Add fallback to prevent certain server-side modules from causing errors on the client side
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+          path: false,
+          os: false,
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;  
